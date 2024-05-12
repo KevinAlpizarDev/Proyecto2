0a9112e34ad215e26466a3c1bb7ff5e0f4fe3075
KevinAlpizarDev <kevinalpizardev@gmail.com>
2024-05-11 21:12:15 -0600

    return

--- src/js/app.js
+++ src/js/app.js
@@ -110,8 +110,8 @@ function readInfo(pic, name, city, email) {
 
 //TAMAÑO DE IMAGEN
 function bigImg(x) {
-    x.style.height = "150px";
-    x.style.width = "150px";
+    x.style.height = "200px";
+    x.style.width = "200px";
 }
 
 function normalImg(x) {
