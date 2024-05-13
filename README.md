1c1ffb35469fc93a84b7e7208cebccb392e08337
KevinAlpizarDev <kevinalpizardev@gmail.com>
2024-05-12 19:27:37 -0600

    LOGIN FINIH

---
+++ README.md
@@ -0,0 +1,4 @@
+const elementoPadre = document.querySelector("#nameMain");
+const nombreUsuario = document.createElement("h1");
+const textoh1 = document.createTextNode("Kevin");
+
--- src/js/signup.js
+++ src/js/signup.js
@@ -1,39 +1,54 @@
-// const signupForm = document.querySelector('#signupForm') //SE TOMA EL FORMULARIO DE REGISTRO POR MEDIO DE ID
-// signupForm.addEventListener('submit', (e) => { //EVENTO SUBMIT, FUNCION CALL BACK 
-//     e.preventDefault() //EVITAR RECARGA DE PAGINA
-//     const name = document.querySelector('#name').value //TOMA VALOR DE LOS CAMPOS INPUT 
-//     const email = document.querySelector('#email').value 
-//     const password = document.querySelector('#password').value
 
-//     const Users = JSON.parse(localStorage.getItem('users')) || [] //AQUÍ SE GUARDAN LOS USUARIOS NUEVOS
-//     const isUserRegistered = Users.find(user => user.email === email) //BUSCA EN LA CONSTANTE LA COINCIDENCIA DE CORREOS
-//     if (isUserRegistered) { //SI ES TRUE
-//         return alert('El usuario ya esta registado!')//YA ESTÁ REGISTRADO
-//     }
+const nameMain = document.querySelector('#nameMain');
 
-//     Users.push({ name: name, email: email, password: password }) //GUARDAMOS UN OBJETO COMPLETO EN EL ARRAY
-//     localStorage.setItem('users', JSON.stringify(Users)) //SE GUARDA EN EL STORAGE "TODOS LOS USERS"
-//     alert('Registro Exitoso!') //ACEPTAR 
-//     window.location.href = 'login.html' //REDIRECCION A LOGIN
 
-// })
+const signupForm = document.querySelector('#signupForm');
 
-const signupForm = document.querySelector('#signupForm')
+const elementoPadre = document.querySelector(".nombre");
+const nombreUsuario = document.createElement("h1");
+const textoh1 = document.createTextNode("Kevin");
+
+
+
+
+
+
+
 signupForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const name = document.querySelector('#name').value
-    const email = document.querySelector('#email').value
-    const password = document.querySelector('#password').value
+    e.preventDefault();
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
 
-    const Users = JSON.parse(localStorage.getItem('users')) || []
-    const isUserRegistered = Users.find(user => user.email === email)
+    const Users = JSON.parse(localStorage.getItem('users')) || [];
+    const isUserRegistered = Users.find(user => user.email === email);
     if (isUserRegistered) {
-        return alert('El usuario ya esta registado!')
+        return alert('El usuario ya está registrado!');
+    }else{
+        
+
+            Users.push({ name: name, email: email, password: password });
+            localStorage.setItem('users', JSON.stringify(Users));
+            alert('Registro Exitoso!');
+            window.location.href = 'login.html';
+        
+            // Imprimir el nombre en el elemento HTML y guardarlo en localStorage
+            const elementoPadre = document.querySelector(".nombre");
+            const nombreUsuario = document.createElement("h1");
+            const textoh1 = document.createTextNode("Kevin");
+            elementoPadre.appendChild(nombreUsuario);
+            nombreUsuario.appendChild(textoh1);
+        
+        
+   
+
+
+
     }
 
-    Users.push({ name: name, email: email, password: password })
-    localStorage.setItem('users', JSON.stringify(Users))
-    alert('Registro Exitoso!')
-    window.location.href = 'login.html'
 
-})
+
+
+});
+
+
